edaac40d51566a9cf81b9f99525109342ecb6fc9
Ozan Kazanç <ozan.kazanc@hotmail.com>
2021-05-10 22:12:57 +0300

    First Commit HRMS

---
+++ src/main/java/kio/HumanResourcesMS/HumanResourcesMsApplication.java
@@ -0,0 +1,13 @@
+package kio.HumanResourcesMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HumanResourcesMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HumanResourcesMsApplication.class, args);
+	}
+
+}
